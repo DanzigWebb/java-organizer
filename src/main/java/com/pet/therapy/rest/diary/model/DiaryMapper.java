06ea44b6a627258214ce2a5759f5464c27b146aa
Alexander <valverin95@gmail.com>
2022-01-30 21:22:46 +0300

    Реализован функционал для парсинга списка моделей

--- src/main/java/com/pet/therapy/rest/diary/model/DiaryMapper.java
+++ src/main/java/com/pet/therapy/rest/diary/model/DiaryMapper.java
@@ -1,10 +1,13 @@
 package com.pet.therapy.rest.diary.model;
 
 import com.pet.therapy.db.entity.DiaryEntity;
+import com.pet.therapy.utils.MapperBase;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class DiaryMapper {
+public class DiaryMapper extends MapperBase {
     public Diary toModel(DiaryEntity entity) {
         var diary = new Diary();
 
@@ -17,5 +20,9 @@ public class DiaryMapper {
         diary.setBodySensation(entity.getBodySensation());
 
         return diary;
+    }
+
+    public List<Diary> listToModel(List<DiaryEntity> entities) {
+        return entityListToModel(entities, this::toModel);
     }
 }
