b90c52cb04f39b63289fbb4c5b652ac412252118
Alexander <valverin95@gmail.com>
2022-02-03 00:24:05 +0300

    feat: добавлен Header

---
+++ client/organizer-client/src/components/template/Header.tsx
@@ -0,0 +1,40 @@
+export default () => {
+    return (
+        <div className="navbar mb-2 shadow-lg bg-neutral text-neutral-content">
+            <div className="flex-1 px-2 mx-2">
+            <span className="text-lg font-bold">
+                daisyUI
+            </span>
+            </div>
+            <div className="flex-none hidden px-2 mx-2 lg:flex">
+                <div className="flex items-stretch">
+                    <a className="btn btn-ghost btn-sm rounded-btn">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                             className="inline-block w-5 mr-2 stroke-current">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"/>
+                        </svg>
+                        Уведомления
+                    </a>
+                    <a className="btn btn-ghost btn-sm rounded-btn">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                             className="inline-block w-5 mr-2 stroke-current">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                  d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"/>
+                        </svg>
+                        Настройка
+                    </a>
+                </div>
+            </div>
+            <div className="flex-none">
+                <button className="btn btn-circle btn-ghost">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                         className="inline-block w-6 h-6 stroke-current">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                              d="M4 6h16M4 12h16M4 18h16"/>
+                    </svg>
+                </button>
+            </div>
+        </div>
+    );
+}
--- client/organizer-client/src/pages/home/Home.tsx
+++ client/organizer-client/src/pages/home/Home.tsx
@@ -1,10 +1,20 @@
 import { Outlet } from "react-router-dom"
+import React, { useEffect } from 'react';
+import { getProfile } from '../../services/api/requests/apiAuthRequest';
+import Header from '../../components/template/Header';
 
 export default () => {
 
+    useEffect(() => {
+        getProfile().then(data => {
+            console.log(data);
+        })
+    }, [])
+
     return (
-        <div>
+        <>
+            <Header />
             <Outlet />
-        </div>
+        </>
     )
 }
