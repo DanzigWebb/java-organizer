4e043c9af1834984b14fdfbc16e278150ebecb55
Alexander <valverin95@gmail.com>
2022-02-03 00:14:55 +0300

    feat: добавлена подстановка токена в интерцепторе

--- client/organizer-client/src/services/api/httpClient.ts
+++ client/organizer-client/src/services/api/httpClient.ts
@@ -1,12 +1,30 @@
-import axios  from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { CONFIG } from '../../config/config';
+import { SigninDataResponse } from './requests/apiAuthRequest';
 
 const httpClient = axios.create({
     baseURL: CONFIG.HOST,
 });
 
 httpClient.interceptors.request.use((config) => {
+    const user = localStorage.getItem('user');
+    if (user) {
+        const data: SigninDataResponse = JSON.parse(user);
+        const token = data.token;
+        setAuthHeader(token, config);
+    }
+
     return config;
 });
+
+function setAuthHeader(token: string, config: AxiosRequestConfig) {
+    if (config.headers) {
+        config.headers.Authorization = `Bearer ${token}`;
+    } else {
+        config.headers = {
+            Authorization: `Bearer ${token}`
+        };
+    }
+}
 
 export default httpClient;
