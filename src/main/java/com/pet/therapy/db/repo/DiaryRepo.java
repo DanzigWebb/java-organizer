8d47fb7a09107ac02bedb51173f6050e94a88c65
Alexander <valverin95@gmail.com>
2022-02-03 22:16:50 +0300

    feat: DiaryRepo: добавлен метод на получение данных в диапазоне дат

--- src/main/java/com/pet/therapy/db/repo/DiaryRepo.java
+++ src/main/java/com/pet/therapy/db/repo/DiaryRepo.java
@@ -2,6 +2,12 @@ package com.pet.therapy.db.repo;
 
 import com.pet.therapy.db.entity.DiaryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Date;
+import java.util.List;
 
 public interface DiaryRepo extends JpaRepository<DiaryEntity, Long> {
+    @Query("SELECT d FROM diary d WHERE d.day >= ?1 AND d.day <= ?2 AND d.user.id = ?3")
+    List<DiaryEntity> getByRange(Date fromDate, Date toDate, Long userId);
 }
