85f514905c00df998b547fcfacc893fba29e4550
Alexander <valverin95@gmail.com>
2022-02-12 12:48:25 +0300

    feat: TabGroup: переход на функциональный стиль

--- client/organizer-client/src/components/tabs/TabGroup.tsx
+++ client/organizer-client/src/components/tabs/TabGroup.tsx
@@ -1,32 +1,37 @@
-import React, { Component, ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 import { TabContextState, TabsContext } from './TabContext';
 
 interface TabGroupProps {
-    children: React.ReactNode;
+    children: ReactNode;
     onChange?: (tabIndex: any) => void;
 }
 
-export class TabGroup extends Component<TabGroupProps, TabContextState> {
+export const TabGroup = (props: TabGroupProps) => {
 
-    state: TabContextState = {
+    const {
+        onChange = () => {},
+        children
+    } = props
+
+    const [state, setState] = useState<TabContextState>({
         activeTab: 0,
-        updateActive: this.updateActiveTab.bind(this),
-        updateActiveContent: this.updateActiveTabContent.bind(this)
-    };
+        updateActive: updateActiveTab,
+        updateActiveContent: updateActiveTabContent
+    });
 
-    private updateActiveTab(index: number) {
-        this.setState((state) => {
+    function updateActiveTab(index: number) {
+        setState((state) => {
             return {
                 ...state,
                 activeTab: index
             };
         });
 
-        this.onChange(index);
+        onChange(index);
     }
 
-    private updateActiveTabContent(node: ReactNode) {
-        this.setState((state) => {
+    function updateActiveTabContent(node: ReactNode) {
+        setState((state) => {
             return {
                 ...state,
                 activeTabContent: node
@@ -34,24 +39,15 @@ export class TabGroup extends Component<TabGroupProps, TabContextState> {
         });
     }
 
+    return (
+        <TabsContext.Provider value={state}>
+            <div className="tabs tabs-boxed pb-2 sticky top-0 z-10">
+                {children}
+            </div>
 
-    // Emit when active tab is change
-    private onChange(index: any) {
-        if (this.props.onChange) {
-            this.props.onChange(index);
-        }
-    }
-
-    render() {
-        return (
-            <TabsContext.Provider value={this.state}>
-                <div className="tabs tabs-boxed pb-2 sticky top-0 z-10">
-                    {this.props.children}
-                </div>
-
-                <div className="tabs-content">
-                    {this.state.activeTabContent}
-                </div>
-            </TabsContext.Provider>);
-    }
-}
+            <div className="tabs-content">
+                {state.activeTabContent}
+            </div>
+        </TabsContext.Provider>
+    );
+};
